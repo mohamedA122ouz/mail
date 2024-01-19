2912b137bd2b78e1bbd9c0ec531997a098b6bb40
prime11 <studyatmohamed@gmail.com>
2024-01-19 19:55:05 +0200

    add some stuff and fix error

--- main.js
+++ main.js
@@ -1,14 +1,39 @@
 import express from "express";
+import fs from "fs";
 const app = express();
 app.use(express.json());
 const port = process.env.PORT || 4000;
 app.listen((port || 4242), "0.0.0.0");
 console.log(port);
 import mail from "./index.js";
+let count = {};
+try {
+    count = fs.readFileSync("./counter.json");
+    count = JSON.parse(count);
+}
+catch {
+    fs.writeFileSync("./counter.json", "{}");
+    count = fs.readFileSync("./counter.json");
+    count = JSON.parse(count);
+}
+function setCount(user) {
+    if (user) {
+        if (count[user] === undefined) {
+            count[user] = 0;
+        }
+        count[user]++;
+        fs.writeFileSync("./counter.json", JSON.stringify(count));
+    }
+    else {
+        throw "undefined username";
+    }
+}
+
+
 app.post("/mail", async (req, res) => {
     try {
-
         let data = req.body;
+        setCount(data.to);
         let i = await mail(data.to, data.subject, data.body);
         if (i) {
             res.status(200);
@@ -23,5 +48,21 @@ app.post("/mail", async (req, res) => {
         console.log(e);
         res.status(500);
         res.json({ msg: "mail not sent" });
+    }
+});
+app.get("/counter", (req, res) => {
+    let u = req.query.username;
+    console.log(u);
+    try {
+        setCount(u);
+        res.status(200);
+        res.json({ msg: count[u]});
+        return;
+    }
+    catch (e) {
+        res.status(404);
+        res.json({ msg: "user not exist" });
+        console.log(e);
+        console.log(req.query);
     }
 });
